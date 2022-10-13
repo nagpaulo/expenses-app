e7857605ca3e5e02c6cf1d9dcd51585c7fe668b4
Paulo Roberto Mesquita da Silva <paulo.roberto@unifor.br>
2022-10-13 18:25:04 -0300

    Refactore componentes

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,38 @@
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/components/transcation_list.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/widgets.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({super.key});
+
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
